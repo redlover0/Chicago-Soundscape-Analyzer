cca2443e8ff2ec339a7c7c894159a0318709e1a5
DhamariTH <dhamaritricehanson@gmail.com>
2025-03-14 21:39:18 -0400

    Set up express request handler and it is now listening on port 3000

---
+++ server.js
@@ -0,0 +1,15 @@
+// We use express to create our server endpoints and listen for and respond to request from the front end
+import express from 'express';
+// We use dot env to access our environment variables
+import "dotenv/config";
+// Create an instance of express
+const requestHandler = express();
+// Storing our port value from the .env file
+const port = process.env.PORT;
+console.log(port);
+// the middle ware that allows us to parse json data.
+requestHandler.use(express.json());
+
+requestHandler.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
