3a3ea49af9f362233697eac668da09a13ae75433
DhamariTH <dhamaritricehanson@gmail.com>
2025-04-17 11:48:17 -0400

    City Page looks cool now, just need to add the pie chart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './routes/Home';
 import './App.css';
-
-
+import CommunityDetails from './routes/CommunityDetails';
 function App() {
     return (
         <div className='container'>
             <Router>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
-                    {/* <Route path="/about" element={} />
-                    <Route path="/contact" element={} /> */}
+                    <Route path="/about" element={<CommunityDetails/>}/>
+                    {/*<Route path="/contact" element={} />*/}
                 </Routes>
             </Router>
         </div>
--- src/componets/TopNavBar.jsx
+++ src/componets/TopNavBar.jsx
@@ -13,8 +13,8 @@ const TopNavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">About Me</Nav.Link>
+                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="about">About Me</Nav.Link>
                         <NavDropdown title="Data Vis" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
