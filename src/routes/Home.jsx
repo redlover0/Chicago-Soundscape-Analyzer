4ec3c061ed298bcc9c932c63b12ac1f6002e4fb1
DhamariTH <dhamaritricehanson@gmail.com>
2025-04-24 17:39:28 -0400

    adding my API and state context

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import TopNavBar from "../componets/TopNavBar";
 import CityCard from "../componets/CityCard";
 import BasicStatsButton from "../componets/BasicStatsButton";
+import Alert from "../componets/alert";
 
 const home = () => {
   return (
     <div>
+        <Alert/>
         <TopNavBar/>
         <BasicStatsButton/>
         <CityCard/>
