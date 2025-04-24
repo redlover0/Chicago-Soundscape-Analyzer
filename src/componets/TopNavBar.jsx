a1244496dbe73a22fe6cce191efcff0602385b78
DhamariTH <dhamaritricehanson@gmail.com>
2025-04-24 17:42:04 -0400

    added chart js

--- src/componets/TopNavBar.jsx
+++ src/componets/TopNavBar.jsx
@@ -7,13 +7,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const TopNavBar = () => {
     return (
-        <Navbar expand="lg" className="navbar navbar-expand-lg navbar-light bg-light">
+        <Navbar expand="lg" className="w-full navbar navbar-expand-lg navbar-light bg-light">
             <Container>
-                <Navbar.Brand href="#home" className='brand'>Safe Sound</Navbar.Brand>
+                <Navbar.Brand href="/" className='brand'>Safe Sound</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="home">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="about">About Me</Nav.Link>
                         <NavDropdown title="Data Vis" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
