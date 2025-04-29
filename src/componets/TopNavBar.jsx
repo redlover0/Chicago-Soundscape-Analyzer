12bf859dc5da85a0bcda69846a3118b30c6ce594
DhamariTH <dhamaritricehanson@gmail.com>
2025-04-29 19:38:30 -0400

    took away drop down menu

--- src/componets/TopNavBar.jsx
+++ src/componets/TopNavBar.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useNavigate} from "react-router-dom";
 
 
 const TopNavBar = () => {
+    let navigate = useNavigate();
+    const navigateToHome = () => {
+        try {
+            navigate('/');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const navigateToAbout = () => {
+        try {
+            navigate('/about');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <Navbar expand="lg" className="w-full navbar navbar-expand-lg navbar-light bg-light">
             <Container>
@@ -13,19 +29,8 @@ const TopNavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="about">About Me</Nav.Link>
-                        <NavDropdown title="Data Vis" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link onClick={navigateToHome}>Home</Nav.Link>
+                        <Nav.Link onClick={navigateToAbout}>About Me</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
